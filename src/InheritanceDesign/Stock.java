6a59a2131bb34e71d8d084cd96b148a4ed51a33b
landmarkallan <gemrose1@gmail.com>
2015-11-10 08:10:44 -0800

    Create Stock.java

---
+++ src/InheritanceDesign/Stock.java
@@ -0,0 +1,13 @@
+public class Stock() extends Security
+{
+	public Stock(String name, String ticker, int quantity, double price)
+	{
+		super(name, ticker, price, quantity)
+	}
+	
+	@Override
+	public toString()
+	{
+		
+	}
+}
