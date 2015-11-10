317ef6dffd711bb742b7f953f1e884d85724351d
landmarkallan <gemrose1@gmail.com>
2015-11-10 08:16:00 -0800

    Create Wealth.java

---
+++ src/InterfaceDesign/Wealth.java
@@ -0,0 +1,38 @@
+public class Wealth()
+{
+	@Override
+	public toString()
+	{
+		
+	}
+	
+	public addAsset()
+	{
+		
+	}
+	
+	public addDebt()
+	{
+		
+	}
+	
+	public getNetWorth()
+	{
+		
+	}
+	
+	public getTotalAssets()
+	{
+		
+	}
+	
+	public getTotalDebts()
+	{
+		
+	}
+	
+	public String getAssetSummary()
+	{
+		
+	}
+}
